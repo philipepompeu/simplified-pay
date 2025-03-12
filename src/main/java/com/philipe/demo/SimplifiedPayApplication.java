d25089de7d0ea695856280b2124736602f9c3759
philipepompeu <philipepompeu2@gmail.com>
2025-03-12 18:41:39 -0300

    Criação inicial da aplicação

---
+++ src/main/java/com/philipe/demo/SimplifiedPayApplication.java
@@ -0,0 +1,13 @@
+package com.philipe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/philipe/demo/SimplifiedPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philipe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
